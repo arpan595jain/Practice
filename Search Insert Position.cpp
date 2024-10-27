5467c3ea7cac53837b0a98d2aca792fce3421693
Arpan Jain <arpanjain595@gmail.com>
2024-10-27 23:18:49 +0530

    Update Search Insert Position.cpp

--- Search Insert Position.cpp
+++ Search Insert Position.cpp
@@ -3,10 +3,8 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
-
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
@@ -15,7 +13,6 @@ public:
                 left = mid + 1;
             }
         }
-
         return left;        
     }
 };
